28fe98bd7090bf914c0df7e4b7b1646153ca5e23
e51033 <e51033@lahsic.com>
2023-02-01 13:52:53 -0600

    Update posts.js

--- _data/posts.js
+++
@@ -1,9 +0,0 @@
-const fetch = require("node-fetch");
-
-module.exports = async function () {
-  console.log("Fetching data...");
-
-  return fetch("https://aaronlandry.net/wp-json/wp/v2/poetry")
-    .then((res) => res.json())
-    .then((json) => json);
-};
